45a886ee72dacb1d89a3059cd98b1a7112ba2619
= <peterlu195@yahoo.com>
2015-10-15 17:12:21 -0700

    classwork 10/15

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,25 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+//when the DOM content has been loaded...
+$(function() {
+    'use strict';
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn();
+    $('#toggle-article').click(function() {
+       $('article').fadeToggle();
+    });
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+    //very quickly gets json results from a url
+    //.then is a **CALLBACK FUNCTION**
+    //call backs are asynchronous function that load after everything
+    $.getJSON(url).then(function(data) {
+        console.log(data);
+        var temperature = data.main.temp;
+        $('#temp').text(Math.round(temperature));
+
+    });
+
+    console.log('test');
+});
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,37 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+ $(function() {
+     'use strict';
+
+     function createMap(loc, zoom) {
+         var map = L.map('map').setView(loc, zoom);
+
+         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+         }).addTo(map);
+
+         //Can add STYLE CLASS and HTML content in the Popup
+         L.circleMarker(loc).addTo(map).bindPopup('<h2>FUCK OFF</h2>').fadeIn();
+     }
+
+     //check to see if browser supports geolocation
+     // have a fall back if they don't
+     if(navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(pos) {
+             createMap([pos.coords.latitude, pos.coords.longitude], 15)
+         });
+
+     } else {
+         createMap([47.6097, -122.3331], 12);
+     }
+
+ });
+
+//get jason
+//wait for it to come back
+//iterate over the array with .forEach or other iterator
+//check lat and long of data obj
+//create map
+//create multiple markers
+//pay attention to who owns camera and adjust style color accordingly
