4d5c288dc29eeaebdc401f9b4a92523b737ecce1
peter <peterlu195@yahoo.com>
2015-11-05 16:59:05 -0800

    class code 11/5

--- angular-forms/js/app.js
+++ angular-forms/js/app.js
@@ -5,13 +5,7 @@
 angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'])
     .constant('storageKey', 'contacts-list')
     .factory('contacts', function(uuid, localStorageService, storageKey) {
-        return[{
-            id: 'default-delete-me',
-            fname: 'Jesus',
-            lname: 'Christ',
-            phone: '777-777-7777',
-            dob: '12/25/0001'
-        }];
+        return localStorageService.get(storageKey) || [];
     })
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -33,17 +27,35 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
 
         $urlRouterProvider.otherwise('/contacts');
     })
+    //register directive for custom validation of dates in the past
+    .directive('intThePast', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.inThePast = function(modelValue) {
+                    var today = new Date();
+                    return (new Date(modelValue) <= today);
+                }
+            }
+        };
+    })
     .controller('ContactsController', function($scope, contacts) {
         $scope.contacts = contacts;
     })
-    .controller('ContactDetailController', function($scope, $stateParams, $state, contacts) {
+    .controller('ContactDetailController', function($scope, $stateParams, $state, contacts, storageKey, localStorageService) {
         $scope.contact = contacts.find(function(contact) {
             //$stateParams.id matches contacts/:id
             //cotact.id matches to id attribute in the original factory function
             return contact.id === $stateParams.id;
         });
+
+        $scope.delete = function() {
+            contacts.pop($scope.contact);
+            localStorageService.set(storageKey, contacts);
+            $state.go('list');
+        }
     })
-    .controller('EditContactController', function($scope, $stateParams, $state, contacts) {
+    .controller('EditContactController', function($scope, $stateParams, $state, uuid, localStorageService, storageKey, contacts) {
         var existingContact = contacts.find(function(contact) {
             //$stateParams.id matches contacts/:id
             //cotact.id matches to id attribute in the original factory function
@@ -52,9 +64,16 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
 
         $scope.contact = angular.copy(existingContact);
 
-        $scope.save = function() {
-            angular.copy($scope.contact, existingContact);
-            $state.go('list');
-        }
+        $scope.saveContact = function() {
+            if ($scope.contact.id) {
+                angular.copy($scope.contact, existingContact);
 
+            } else {
+                $scope.contact.id = uuid.v4();
+                contacts.push($scope.contact);
+            }
+            localStorageService.set(storageKey, contacts);
+
+            $state.go('list');
+        };
     });
